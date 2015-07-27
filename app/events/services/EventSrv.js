f58382b739949db1785dd469fa3a2372d0307d42
Yann Lerjen <yann.lerjen@gmail.com>
2015-07-27 20:10:12 +0200

    api path correction

--- app/events/services/EventSrv.js
+++ app/events/services/EventSrv.js
@@ -1,14 +1,14 @@
 classe84App.factory('evtSrv', function($http){
 	return{
 		getEvents : function(){
-			return $http.get(appSettings.apiUrl + '/api/events');
+			return $http.get(appSettings.apiUrl + '/events');
 		},
 		getEventById: function(id){
-			return $http.get(appSettings.apiUrl + '/api/events/'+id);
+			return $http.get(appSettings.apiUrl + '/events/' + id);
 		},
 		saveEvent: function(event){
 			throw 'Not Implemented Exception';
-			return $http.post(appSettings.apiUrl + '/api/')
+			return $http.post(appSettings.apiUrl + '/')
 		}
 	};
 });
