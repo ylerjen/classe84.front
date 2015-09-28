c90ea0fdd170d9aef38704963a3a4e9dcd5392e1
Yann Lerjen <yann.lerjen@gmail.com>
2015-09-28 21:46:41 +0200

    documentation

--- app/events/controllers/EventCtrl.js
+++ app/events/controllers/EventCtrl.js
@@ -1,25 +1,22 @@
-"use strict";
-
 angular.module('84.events')
 
 .controller('EventListCtrl', ['$scope', '$http', 'evtSrv', function ($scope, $http, evtSrv) {
-	
+	"use strict";
     evtSrv.getEvents().success(function(events){
         $scope.events = events;
     }).error(function(e){
-		console.log('Error lors de la récupération des events : ' + e);
+		console.error('Error lors de la récupération des events : ' + e);
     }); 
-
-  
 }])
 
 /**
  * Manage the event detail and event form for the show/edit/add functionnalities
- * @param  {[type]} $scope       angular scope dependancy injection
- * @param  {[type]} $routeParams angular routing dependancy injection
- * @param  {[type]} evtSrv)      event services dependancy injection
+ * @param  {object} $scope       angular scope dependancy injection
+ * @param  {object} $routeParams angular routing dependancy injection
+ * @param  {object} evtSrv       event services dependancy injection
  */
 .controller('EventDetailCtrl', ['$scope', '$routeParams', 'evtSrv', function($scope, $routeParams, evtSrv) {
+	"use strict";
     var eventId = $routeParams.eventId;
 	
     evtSrv.getEventById(eventId).success(function(event){
