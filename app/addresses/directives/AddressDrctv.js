8c32070755d2c79a37b62809d784b4e6ec799e76
Yann Lerjen <yann.lerjen@gmail.com>
2015-12-13 21:13:08 +0100

    minor changes changed formatting

--- app/addresses/directives/AddressDrctv.js
+++ app/addresses/directives/AddressDrctv.js
@@ -1,7 +1,7 @@
 // Directives
-'use strict';
 angular.module('84.addresses')    
     .directive('addresses', [function () {
+    'use strict';
         return {
             replace : true,
             restrict : 'A',
@@ -12,11 +12,12 @@ angular.module('84.addresses')
             link : function (scope, element, attrs) {
                 scope.$watch('addressesAttr', function(newValue, oldValue) {
                    console.log(newValue);
-                })
+                });
             }
         };
     }])
     .directive('address', [ function () {
+        'use strict';
         return {
             restrict : 'A',
             templateUrl : '/app/addresses/views/address.html',
@@ -26,7 +27,7 @@ angular.module('84.addresses')
             link : function (scope, element, attrs) {
                 scope.$watch('addressAttr', function(newValue, oldValue) {
                    console.log(newValue);
-                })
+                });
             }
         };
     }]);
