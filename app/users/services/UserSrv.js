4ed68b0d0fc810444cf54b33a9b343ef6518f912
Yann Lerjen <yann.lerjen@gmail.com>
2015-12-13 21:12:41 +0100

    minor changes changed formatting

--- app/users/services/UserSrv.js
+++ app/users/services/UserSrv.js
@@ -1,28 +1,31 @@
-angular.module('84.users').factory('usrSrv', ['$http', 'API_URL', function ($http, API_URL) {
-	return {
-		getUsers : function(){
-			return $http.get(API_URL.api84 + '/users');
-		},
-		findById: function(id){
-			return $http.get(API_URL.api84 + '/users/' + id);
-		},
-		saveUser: function(user){
-            if(user.id) {
-                return $http.put(API_URL.api84 + '/users/' + user.id, user);
-            } else {
-                return $http.post(API_URL.api84 + '/users', user);
-            }
+angular.module('84.users')
+    .factory('usrSrv', ['$http', 'API_URL', function ($http, API_URL) {
+    	return {
+    		getUsers : function(){
+    			return $http.get(API_URL.api84 + '/users');
+    		},
+    		findById: function(id){
+    			return $http.get(API_URL.api84 + '/users/' + id);
+    		},
+    		saveUser: function(user){
+                if(user.id) {
+                    return $http.put(API_URL.api84 + '/users/' + user.id, user);
+                } else {
+                    return $http.post(API_URL.api84 + '/users', user);
+                }
 
-		},
-        deleteUser: function (id){
-            return $http.delete(API_URL.api84 + '/users/' + id);
+    		},
+            deleteUser: function (id){
+                return $http.delete(API_URL.api84 + '/users/' + id);
+            }
+    	};
+    }])
+    .factory('fbSrv', ['$http', 'API_URL', function ($http, API_URL) {
+        return {
+            getFbSilouetteUrl : function (fbId) {           
+                var fbUrl = API_URL.fbApiUrl + fbId + '/picture?type=normal&redirect=false';
+                return $http.get(fbUrl);
+            }
         }
-	};
-}]).factory('fbSrv', ['$http', 'API_URL', function ($http, API_URL) {
-    return {
-        getFbSilouetteUrl : function (fbId) {           
-            var fbUrl = API_URL.fbApiUrl + fbId + '/picture?type=normal&redirect=false';
-            return $http.get(fbUrl);
-        }
-    }
-}]);
+    }]
+);
