73f4ecbe7c9f981b2a2d40592bd00f2c5c1ad90f
Yann Lerjen <yann.lerjen@gmail.com>
2016-12-18 19:16:44 +0100

    manage active state for navigation component

--- src/js/nav/NavigationComponent.js
+++ src/js/nav/NavigationComponent.js
@@ -1,6 +1,26 @@
+const menuItems = [
+  'about',
+  'user',
+  'event',
+  'admin',
+  'gallery',
+  'contact'
+];
+
+class NavigationController {
+    constructor() {
+        this.navItems = menuItems;
+        this.activeTab = 0;
+    }
+    isActive(index) {
+        return this.activeTab === index;
+    }
+    select(index) {
+        this.activeTab = index;
+    }
+}
+
 export default {
     templateUrl: 'src/js/nav/views/nav.html',
-    controller: () => {
-        
-    }
+    controller: NavigationController
 }
