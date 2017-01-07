6aa720ba78a68c581c845d0806e0da80fd1c432a
Yann Lerjen <yann.lerjen@gmail.com>
2017-01-07 14:57:05 +0100

    add a next birthday component (not used yet)

---
+++ src/js/components/NextBirthdayComponent.js
@@ -0,0 +1,10 @@
+class NextBirthdayController {
+    constructor() {
+
+    }
+}
+
+export default {
+    templateUrl: '/tpl/widget/next-birthday-widget.html',
+    controller: NextBirthdayController
+};
