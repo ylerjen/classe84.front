8256e4fd882ce37f199fe57954c36511f7b119e1
Yann Lerjen <yann.lerjen@gmail.com>
2017-05-01 22:37:29 +0200

    bind the filter to the list

--- src/app/components/user-list-wrapper/user-list-wrapper.component.ts
+++ src/app/components/user-list-wrapper/user-list-wrapper.component.ts
@@ -15,23 +15,34 @@ export class UserListWrapperComponent {
     private filter = '';
     private isLoading = false;
     private usersList: User[] = [];
-
+    private filteredList: User[] = [];
 
     constructor(private usersService: UsersService, private _store: Store<IUserState>) {
         _store.select('userList')
             .subscribe( (uState: IUserState) => {
                 if (uState) {
                     this.usersList = uState.userList;
+                    this.filteredList = uState.userList;
                     this.isLoading = uState.isLoading;
                 }
             });
+    }
+
+    filterList(filter) {
+        const lcFilter = filter.toLowerCase();
+        this.filteredList = this.usersList.filter(user => {
+            return (user.first_name.toLowerCase().includes(lcFilter) ||
+                user.last_name.toLowerCase().includes(lcFilter) ||
+                (user.maiden_name && user.maiden_name.toLowerCase().includes(lcFilter)) );
+        });
     }
 
     reloadAll() {
         this.usersService.reload();
     }
 
-    filterChange(param) {
-        console.log(param);
+    filterChange(evt) {
+        const filterStr = evt.target.value;
+        this.filterList(filterStr);
     }
 }
