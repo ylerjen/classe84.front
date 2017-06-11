8a74738c3f0c105839c6a7797e716dc96a3a87ca
Yann Lerjen <yann.lerjen@gmail.com>
2017-06-11 22:39:54 +0200

    routes are exported as an object

--- src/app/config/router.config.ts
+++ src/app/config/router.config.ts
@@ -1,25 +1,39 @@
 import { Routes } from '@angular/router';
+
 import { HomePage } from '../pages/home-page/home.page';
 import { AboutPage } from '../pages/about-page/about.page';
 import { UsersPage } from '../pages/userlist-page/users.page';
-import { UserPage } from '../pages/user-page/user-page';
+import { LoginPage } from '../pages/login-page/login.page';
+import { UserPage } from '../pages/user-page/user.page';
+
+export const ROUTE_URL: { [key: string]: string } = {
+    default: '',
+    about: 'about',
+    login: 'login',
+    events: 'events',
+    users: 'users',
+    userById: 'users/:id'
+};
 
 export const appRoutes: Routes = [
     {
-        path: '',
+        path: ROUTE_URL.default,
         component: HomePage
     }, {
-        path: 'users/:id',
+        path: ROUTE_URL.userById,
         component: UserPage
     }, {
-        path: 'users',
+        path: ROUTE_URL.users,
         component: UsersPage
     }, {
-        path: 'events',
+        path: ROUTE_URL.events,
         component: UsersPage
     }, {
-        path: 'about',
+        path: ROUTE_URL.about,
         component: AboutPage
+    }, {
+        path: ROUTE_URL.login,
+        component: LoginPage
     }/*, {
         path: 'heroes',
         component: HeroListComponent,
