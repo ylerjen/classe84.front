eb2be518da21ab0e6bddb25a5765f5ca31a6b473
Yann Lerjen <yann.lerjen@gmail.com>
2017-07-05 22:33:11 +0200

    add version from env

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent { }
+export class FooterComponent {
+    public frontVersion: string = environment.version;
+    public apiVersion: string = 'x.x.x';
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
+const { version: appVersion } = require('../../package.json');
+
 export const environment = {
   production: true,
   API_URL: 'http://api.classe84.org',
-  version: '0.0.0'
+  version: appVersion
 };
