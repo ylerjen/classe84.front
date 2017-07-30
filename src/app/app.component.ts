7269f7050fbf1edff382fd0be9241dc269e99084
Yann Lerjen <yann.lerjen@gmail.com>
2017-07-30 16:37:15 +0200

    get login state on app initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { environment } from '../environments/environment';
 import { AppState, AppVersion } from './stores/app/appReducer';
 import { storeFrontVersion, storeApiVersion } from './actions/app.actions';
 import { AppService } from './services/app/app.service';
+import { LoginService } from './services/login/login.service';
+import { login } from './actions/session.actions';
 
 @Component({
     selector: 'app-root',
@@ -15,33 +17,30 @@ export class AppComponent implements OnInit {
 
     public _version: AppVersion;
 
-    set version(val) { console.log('setter', val); this._version = val; }
+    set version(val) { this._version = val; }
     get version() { return this._version; }
 
     constructor(
         private _appSrvc: AppService,
         private _store: Store<AppState>,
+        private _loginSrvc: LoginService
     ) {
         this._store.select('appState')
             .subscribe(
-                (resp: AppState) => {
-                    const v = resp.version;
-                    console.log('from store', v);
-                    this.version = v;
-                },
+                (resp: AppState) => this.version = resp.version,
                 (err) => console.error(err),
                 () => console.log('on finish')
             );
-    }
+        }
 
-
-    ngOnInit(): void {
-        this._store.dispatch(storeFrontVersion(environment.version));
-        this._appSrvc.getApiVersion().subscribe(
-            (resp) => {
-                console.log('from api', resp);
-                this._store.dispatch(storeApiVersion(resp));
+        ngOnInit(): void {
+            this._store.dispatch(storeFrontVersion(environment.version));
+            this._appSrvc.getApiVersion().subscribe(
+                (resp) => this._store.dispatch(storeApiVersion(resp))
+            );
+            const token = this._loginSrvc.getTokenFromStorage();
+            if (token) {
+                this._store.dispatch(login(token));
             }
-        );
     }
 }
