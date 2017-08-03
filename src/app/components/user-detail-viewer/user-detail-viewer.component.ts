440f4b22bfc527b186bdb70089675bbb8b3f6523
Yann Lerjen <yann.lerjen@gmail.com>
2017-08-03 16:17:29 +0200

    use component lifecycle to un/-subscribe observable

--- src/app/components/user-detail-viewer/user-detail-viewer.component.ts
+++ src/app/components/user-detail-viewer/user-detail-viewer.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 import { IUserState } from '../../stores/user/userReducer';
 import { User } from '../../models/User';
@@ -11,16 +14,26 @@ import { ROUTE_URL } from '../../config/router.config';
     templateUrl: './user-detail-viewer.component.html',
     styleUrls: ['./user-detail-viewer.component.scss']
 })
-export class UserDetailViewerComponent {
+export class UserDetailViewerComponent implements OnInit, OnDestroy {
 
     public user: User;
+
+    private store$: Observable<IUserState>;
+    private sub: Subscription;
 
     constructor(
         private _store: Store<IUserState>,
         private _router: Router
     ) {
-        this._store.select('userState')
-            .subscribe((userState: IUserState) => this.user = userState.user);
+        this.store$ = this._store.select('userState');
+    }
+
+    ngOnInit(): void {
+        this.sub = this.store$.subscribe((userState: IUserState) => this.user = userState.user);
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 
     goToEdit(id: number): void {
@@ -32,6 +45,6 @@ export class UserDetailViewerComponent {
     }
     delete(id: number): void {
         console.log('delete', id);
-
+        throw new Error('not implemented yet');
     }
 }
