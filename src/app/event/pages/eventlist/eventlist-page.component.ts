b1550edf8d5f1a74e6179b3d56a0d7ff0cc54028
Yann Lerjen <yann.lerjen@gmail.com>
2017-08-09 01:04:33 +0200

    create events components

--- src/app/event/pages/eventlist/eventlist-page.component.ts
+++ src/app/event/pages/eventlist/eventlist-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { EventsService } from '../../services/events.service';
 
 @Component({
-  selector: 'app-eventlist-page',
-  templateUrl: './eventlist-page.component.html',
-  styleUrls: ['./eventlist-page.component.scss']
+    selector: 'app-eventlist-page',
+    templateUrl: './eventlist-page.component.html',
+    styleUrls: ['./eventlist-page.component.scss']
 })
 export class EventlistPageComponent implements OnInit {
 
-  constructor() { }
+    public eventList = [];
 
-  ngOnInit() {
-  }
+    constructor(private _evtSrvc: EventsService) { }
+
+    ngOnInit() {
+        this._evtSrvc.fetchAll()
+            .subscribe(
+                (resp) => this.eventList = resp
+            );
+    }
 
 }
