7842e1b6b7a90d1d3c104c47a1cb685ddd96461c
Yann Lerjen <yann.lerjen@gmail.com>
2017-08-09 01:03:33 +0200

    create event class

--- src/app/models/Event.ts
+++ src/app/models/Event.ts
@@ -1,3 +1,15 @@
 export class Event {
+    public id: number;
+    public title: string;
+    public event_date: Date;
+    public location: string;
+    public organisator: string;
 
+    constructor(props: { [key: string]: any } = {}) {
+        this.id = props.id;
+        this.title = props.title;
+        this.event_date = props.event_date;
+        this.location = props.location;
+        this.organisator = props.organisator;
+    }
 }
