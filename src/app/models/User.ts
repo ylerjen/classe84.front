8cbbe26525f68539f365de194ec2ba1e959b748f
Yann Lerjen <yann.lerjen@gmail.com>
2017-08-08 18:42:55 +0200

    add constructor with passed props

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -3,6 +3,9 @@ export enum EGender {
     Female
 }
 
+/**
+ * A user object that describe a member person
+ */
 export class User {
     public id: number;
     public last_name: string;
@@ -18,12 +21,50 @@ export class User {
     public fb_profile_name: string;
     public website: string;
 
-    get fullName() {
-        let name = this.last_name;
+    get fullname(): string {
+        let name = '';
+        if (this.last_name) {
+            name += this.last_name;
+        }
         if (this.maiden_name) {
             name += ` (${this.maiden_name})`;
         }
-        name += ` ${this.last_name}`;
+        if (this.first_name) {
+            name += ` ${this.first_name}`;
+        }
         return name;
+    }
+
+    constructor(props: { [key: string]: any } = {}) {
+        this.id = props.id;
+        this.last_name = props.last_name;
+        this.maiden_name = props.maiden_name;
+        this.first_name = props.first_name;
+        this.gender = props.gender;
+        this.birthdate = props.birthdate;
+        this.is_active = props.is_active;
+        this.email = props.email;
+        this.phone = props.phone;
+        this.mobile = props.mobile;
+        this.fb_user_id = props.fb_user_id;
+        this.fb_profile_name = props.fb_profile_name;
+        this.website = props.website;
+    }
+
+    /**
+     * Calculate the age of the person at a given date
+     * @param [refDate=now()] is the date used to calculate the age. If none is passed, current date is used.
+     * @returns {number} the calculated age
+     */
+    getAge(refDate: Date): number {
+        if (typeof refDate === 'undefined') {
+            refDate = new Date();
+        }
+        let age = refDate.getFullYear() - this.birthdate.getFullYear();
+        const m = refDate.getMonth() - this.birthdate.getMonth();
+        if (m < 0 || (m === 0 && refDate.getDate() < this.birthdate.getDate())) {
+            age--;
+        }
+        return age;
     }
 }
