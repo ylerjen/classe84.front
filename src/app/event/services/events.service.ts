17a6d2e6a8e4d3573b6b0fe515602b3e7432caf6
Yann Lerjen <yann.lerjen@gmail.com>
2017-08-10 01:31:54 +0200

    add get event and getParticipants

--- src/app/event/services/events.service.ts
+++ src/app/event/services/events.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { AuthHttp } from 'angular2-jwt';
 
 import { environment as env } from '../../../environments/environment';
 import { Event } from '../../models/Event';
+import { User } from '../../models/User';
+
 const BASE_URL = `${env.API_URL}/events`;
 
 @Injectable()
 export class EventsService {
 
-    constructor(private _http: Http) { }
+    constructor(private _http: AuthHttp) { }
 
     fetchAll(): Observable<Array<Event>> {
         return this._http.get(BASE_URL)
             .map((resp: Response): Array<Event> => resp.json());
     }
 
+    get(id: number): Observable<Event> {
+        return this._http.get(`${BASE_URL}/${id}`)
+            .map((resp: Response): Event => new Event(resp.json()))
+            .catch((error: any) => {
+                if (error.status < 400 ||  error.status === 500) {
+                    return Observable.throw(new Error(error.status));
+                }
+            });
+    }
+
+    getParticipants(id: number): Observable<Array<User>> {
+        return this._http.get(`${BASE_URL}/${id}/users`)
+            .map((resp: Response): Array<User> => resp.json());
+    }
 }
