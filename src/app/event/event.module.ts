55e03aa423f1b4ac8a1142429ea06384c997f3e3
Yann Lerjen <yann.lerjen@gmail.com>
2017-08-10 01:32:11 +0200

    clean module

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -2,35 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AgmCoreModule } from '@agm/core';
+
+import { GMAP_API_KEY } from '../config/settings';
 import { SharedModule } from '../shared/shared.module';
+import { AuthService } from '../services/auth/auth.service';
 import { EventsService } from './services/events.service';
 import { EventlistPageComponent } from './pages/eventlist/eventlist-page.component';
 import { EventPageComponent } from './pages/event/event-page.component';
 import { EventListComponent } from './event-list/event-list.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventListWrapperComponent } from './event-list-wrapper/event-list-wrapper.component';
+import { EventListFilterComponent } from './event-list-filter/event-list-filter.component';
 
 const eventRoutes: Routes = [
-    { path: 'events', component: EventlistPageComponent},
-    // {
-    //     path: ROUTE_URL.eventById,
-    //     component: EventPageComponent,
-    //     canActivate: [ AuthService ],
-    //     children: [
-    //         {
-    //             path: ROUTE_URL.default,
-    //             pathMatch: 'full',
-    //             component: UserDetailViewerComponent,
-    //             children: [
-    //                 { path: ROUTE_URL.address, component: LoginPage },
-    //                 { path: ROUTE_URL.contact, component: AboutPage },
-    //             ]
-    //         },
-    //         { path: ROUTE_URL.edit, component: UserFormViewerComponent },
-    //     ]
-    // },
-    // {
-    //     path: ROUTE_URL.events,
-    //     component: EventlistPageComponent
-    // },
+    { path: 'events', component: EventlistPageComponent },
+    { path: 'events/:id', component: EventPageComponent, canActivate: [ AuthService ] }
 ];
 
 @NgModule({
@@ -38,11 +25,17 @@ const eventRoutes: Routes = [
         CommonModule,
         SharedModule,
         RouterModule.forChild(eventRoutes),
+        AgmCoreModule.forRoot({
+            apiKey: GMAP_API_KEY
+          }),
     ],
     declarations: [
         EventlistPageComponent,
         EventPageComponent,
         EventListComponent,
+        EventDetailComponent,
+        EventListWrapperComponent,
+        EventListFilterComponent,
     ],
     providers: [
         EventsService
