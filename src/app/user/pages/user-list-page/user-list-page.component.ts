74699a3751f105dda8f3916302d6e9f2b83b573d
Yann Lerjen <yann.lerjen@gmail.com>
2017-08-15 18:09:49 +0200

    add true value for user list stats

--- src/app/user/pages/user-list-page/user-list-page.component.ts
+++ src/app/user/pages/user-list-page/user-list-page.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 
-import { User } from '../../../models/User';
+import { User, EGender } from '../../../models/User';
+import { IUserListState } from '../../../stores/userlist/userlistReducer';
 import { IUserState } from '../../../stores/user/userReducer';
 import { UsersService } from '../../../services/users/users.service';
 
@@ -15,13 +16,31 @@ import { UsersService } from '../../../services/users/users.service';
 export class UserListPageComponent implements OnInit {
 
     public nextBirthdayUsers: Array<User> = [];
+    public totalActive = 0;
+    public totalMembers = 0;
+    public totalMen = 0;
+    public totalWomen = 0;
 
-    constructor(private _userSrvc: UsersService) {}
+
+    constructor(
+        private _userSrvc: UsersService,
+        private _store: Store<IUserListState>,
+    ) {}
 
     ngOnInit(): void {
         this._userSrvc.reload();
         this._userSrvc.fetchNextBirthday()
             .subscribe( (userList: Array<User>) => this.nextBirthdayUsers = userList);
+        this._store.select('userlistState')
+            .subscribe( (uState: IUserListState) => {
+                if (uState) {
+                    const usersList = uState.userList;
+                    this.totalMembers = usersList.length;
+                    this.totalActive = usersList.filter((user: User) => user.is_active).length;
+                    this.totalMen = usersList.filter((user: User) => user.gender === EGender.Male).length;
+                    this.totalWomen = this.totalMembers - this.totalMen;
+                }
+            });
     }
 
     addUser(user: User) {
