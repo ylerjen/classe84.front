af5446a93330b7d5a4ce30e779458da3046ecca8
Yann Lerjen <yann.lerjen@gmail.com>
2017-09-12 23:22:48 +0200

    fix version to string

--- src/app/models/Version.ts
+++ src/app/models/Version.ts
@@ -2,7 +2,7 @@ export class Version {
     public major: number;
     public minor: number;
     public patch: number;
-    public rev: number;
+    public rev: string;
 
     static fromObj(version: { major, minor, patch }): Version {
         const v = new Version();
@@ -18,10 +18,14 @@ export class Version {
         this.major = verSplit.length > 0 ? +verSplit[0] : defaultVersionSegment;
         this.minor = verSplit.length > 1 ? +verSplit[1] : defaultVersionSegment;
         this.patch = verSplit.length > 2 ? +verSplit[2] : defaultVersionSegment;
-        this.rev = verSplit.length > 3 ? +verSplit[3] : defaultVersionSegment;
+        this.rev = verSplit.length > 3 ? verSplit[3] : '';
     }
 
     public get full(): string {
-        return `${this.major.toString()}.${this.minor.toString()}.${this.patch.toString()}.${this.rev.toString()}`;
+        let version = `${this.major.toString()}.${this.minor.toString()}.${this.patch.toString()}`;
+        if (this.rev) {
+            version += `.${this.rev}`;
+        }
+        return version;
     }
 }
