b62debc76dcf6a4255eb9c8cb3db9ffa760eb677
Yann Lerjen <yann.lerjen@gmail.com>
2017-09-13 21:37:43 +0200

    add doc + fix wrong http use

--- src/app/event/services/events.service.ts
+++ src/app/event/services/events.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { AuthHttp } from 'angular2-jwt';
 
@@ -12,15 +12,24 @@ const BASE_URL = `${env.API_URL}/events`;
 @Injectable()
 export class EventsService {
 
-    constructor(private _http: AuthHttp) { }
+    constructor(
+        private _http: Http,
+        private _authHttp: AuthHttp) { }
 
+    /**
+     * Fetch all events
+     */
     fetchAll(): Observable<Array<Event>> {
         return this._http.get(BASE_URL)
             .map((resp: Response): Array<Event> => resp.json());
     }
 
+    /**
+     * Get the event by its id
+     * @param id - the id of the event to get
+     */
     get(id: number): Observable<Event> {
-        return this._http.get(`${BASE_URL}/${id}`)
+        return this._authHttp.get(`${BASE_URL}/${id}`)
             .map((resp: Response): Event => new Event(resp.json()))
             .catch((error: any) => {
                 if (error.status < 400 ||  error.status === 500) {
@@ -29,8 +38,12 @@ export class EventsService {
             });
     }
 
-    getParticipants(id: number): Observable<Array<User>> {
-        return this._http.get(`${BASE_URL}/${id}/users`)
+    /**
+     * Get all subscribers for an event
+     * @param eventId - the id of the event
+     */
+    getParticipants(eventId: number): Observable<Array<User>> {
+        return this._authHttp.get(`${BASE_URL}/${eventId}/users`)
             .map((resp: Response): Array<User> => resp.json());
     }
 }
