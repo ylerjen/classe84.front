e802c0fd73b410b0cd7a68f8248aa5e8686c0b2d
Yann Lerjen <yann.lerjen@gmail.com>
2017-09-18 22:17:16 +0200

    finish the contact form

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { ReCaptchaModule } from 'angular2-recaptcha';
+import { SharedModule } from '../shared/shared.module';
 import { ContactService } from './services/contact.service';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
@@ -9,7 +11,9 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
 @NgModule({
     imports: [
         CommonModule,
+        SharedModule,
         ReCaptchaModule,
+        ReactiveFormsModule,
     ],
     declarations: [
         ContactPageComponent,
