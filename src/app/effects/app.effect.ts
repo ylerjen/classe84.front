06ce83dc6f223e6e6db395e5f776221f029453ad
Yann Lerjen <yann.lerjen@gmail.com>
2018-04-01 16:06:32 +0200

    add effect fro app version

---
+++ src/app/effects/app.effect.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect } from '@ngrx/effects';
+import { Observable } from 'rxjs/Observable';
+import { AppActions, getApiVersionFinished } from 'app/actions/app.actions';
+import { addNotif } from 'app/actions/notifications.actions';
+import { AppService } from '../services/app/app.service';
+import { Version } from 'app/models/Version';
+import { Notification, ENotificationType } from 'app/models/Notification';
+
+@Injectable()
+export class AddressEffects {
+
+    @Effect()
+    getApiVersion$ = this.actions$
+        .ofType(AppActions.getApiVersion)
+        .switchMap(payload => this._appSrvc.getApiVersion()
+        .map((res: Version) => getApiVersionFinished(res))
+        .catch((err: Error) => {
+            const errNotif = new Notification(JSON.stringify(err), ENotificationType.ERROR);
+            return Observable.of(addNotif(errNotif));
+        })
+    );
+
+
+    constructor(
+        private actions$: Actions,
+        private _appSrvc: AppService
+    ) {}
+}
