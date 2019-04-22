226b9b0c349a9849191e0179b95baf044b7e8b94
Yann Lerjen <yann.lerjen@gmail.com>
2019-04-22 23:59:46 +0200

    complete session object

--- src/app/models/Session.ts
+++ src/app/models/Session.ts
@@ -4,14 +4,17 @@ import { User } from './User';
  * A session object used when a user has logged in
  */
 export class Session {
-
     public loginTime: Date;
     public token: string;
+    public tokenType: string;
+    public expiresIn: number;
     public user: User;
 
     constructor(opts: { [key: string]:  any }) {
         this.loginTime = opts.loginTime || new Date();
         this.token = opts.token;
+        this.tokenType = opts.tokenType
+        this.expiresIn = opts.expiresIn;
         this.user = opts.user ? new User(opts.user) : null;
     }
 }
