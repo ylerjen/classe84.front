7dcf70342a01c2a9b3b220edc848ae27af9fb8fa
Yann Lerjen <yann.lerjen@gmail.com>
2019-06-02 23:28:09 +0200

    Add forbidden page

---
+++ src/app/pages/forbidden/forbidden.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'forbidden-page',
+  templateUrl: './forbidden.page.html',
+  styleUrls: ['./forbidden.page.scss']
+})
+// tslint:disable-next-line:component-class-suffix
+export class ForbiddenPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
