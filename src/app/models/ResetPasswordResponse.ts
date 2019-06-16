6beca63c1bb654e88ecfc7fa50d05c6bd992a802
Yann Lerjen <yann.lerjen@gmail.com>
2019-06-16 23:07:23 +0200

    create interface

---
+++ src/app/models/ResetPasswordResponse.ts
@@ -0,0 +1,8 @@
+export interface ResetPasswordResponse {
+    email: string;
+    frontUrl: string;
+    message: string;
+    recoveryUrl: string;
+    token: string;
+    tokenPlaceholder: string;
+}
