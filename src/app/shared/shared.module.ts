159df8d9851acd35f83a8c1161963b2167ee1faa
Yann Lerjen <yann.lerjen@gmail.com>
2019-11-17 23:22:47 +0100

    fix wrong import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,9 +12,8 @@ import { GeoService } from './services/geo/geo.service';
 import { NotificationService } from './services/notification/notification.service';
 import { DropdownComponent } from './dropdown/dropdown.component';
 import { ModalComponent } from './modal/modal.component';
-import { ItemControlsComponent } from './item-controls/item-controls.component';
 import { CoordinatesFormComponent } from './coordinates-form/coordinates-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { StaticMapComponent } from './static-map/static-map.component';
 
 @NgModule({
@@ -39,7 +38,6 @@ import { StaticMapComponent } from './static-map/static-map.component';
         FlipComponent,
         DropdownComponent,
         ModalComponent,
-        ItemControlsComponent,
         CoordinatesFormComponent,
         StaticMapComponent,
     ],
@@ -57,7 +55,6 @@ import { StaticMapComponent } from './static-map/static-map.component';
         FlipComponent,
         DropdownComponent,
         ModalComponent,
-        ItemControlsComponent,
         CoordinatesFormComponent,
         StaticMapComponent,
     ],
